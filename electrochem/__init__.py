67febe5a15aa35b9a9616e0743cb62a75ca7ece0
Tyler Pennebaker <pennebaker@ucsb.edu>
2026-07-11 18:48:08 -0700

    Bump version: 0.1.1 → 0.2.0

--- electrochem/__init__.py
+++ electrochem/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Vincent Wu"""
 __email__ = 'vincentwu@ucsb.edu'
-__version__ = '0.1.1'
+__version__ = '0.2.0'
 from electrochem.parse import (extractCycleEchem, extractEchem,
                                generateEchemSummary, generateSummary,
                                parseArbin, plotEchem, toDataframe)
